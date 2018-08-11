7615d20c158561795602d807adcae3fd35005928
Jörg Thalheim <joerg@thalheim.io>
2018-08-11 11:26:17 +0200

    update channel code

---
+++ default.nix
@@ -0,0 +1,25 @@
+{ nurpkgs ? import <nixpkgs> {} # For nixpkgs dependencies used by NUR itself
+  # Dependencies to call NUR repos with
+, pkgs ? null }:
+
+let
+  manifest = (builtins.fromJSON (builtins.readFile ./repos.json)).repos;
+  lockedRevisions = (builtins.fromJSON (builtins.readFile ./repos.json.lock)).repos;
+
+  inherit (nurpkgs) lib;
+
+  repoSource = name: attr: import ./lib/repoSource.nix {
+    inherit name attr manifest lockedRevisions lib;
+    inherit (nurpkgs) fetchgit fetchzip;
+  };
+
+  createRepo = name: attr: import ./lib/evalRepo.nix {
+    inherit name pkgs lib;
+    inherit (attr) url;
+    src = repoSource name attr + "/" + (attr.file or "");
+  };
+
+in {
+  repos =  lib.mapAttrs createRepo manifest;
+  repo-sources = lib.mapAttrs repoSource manifest;
+}
